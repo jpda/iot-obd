9e12c7e7e245bc5eb4cb3ae62ae4e356e97946b1
Jack Bradham <jackb@live.com>
2016-03-07 13:37:43 -0500

    chage

--- Parkwood.Obd/IotSubscriber.cs
+++ Parkwood.Obd/IotSubscriber.cs
@@ -4,23 +4,7 @@ namespace Parkwood.Obd
 {
     public class IotSubscriber : StateSubscriber
     {
-        private IDisposable _unsubscriber;
 
-        public IotSubscriber()
-        {
-           
-        }
-
-        public string Name { get; }
-        public override void OnCompleted()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void OnError(Exception error)
-        {
-            throw new NotImplementedException();
-        }
 
         public override async void OnNext(State value)
         {
